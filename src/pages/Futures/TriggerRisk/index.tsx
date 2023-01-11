6ef7261578e09017ae48c9811ea57bac4be845c0
DoctorLi <doctorli2042@gmail.com>
2023-01-11 17:16:45 +0800

    update

--- src/pages/Futures/TriggerRisk/index.tsx
+++ src/pages/Futures/TriggerRisk/index.tsx
@@ -20,7 +20,13 @@ const TriggerRiskModal: FC<Props> = ({ ...props }) => {
         <Stack spacing={0} alignItems="left" className={`${classPrefix}-main`}>
           <p className={`${classPrefix}-main-title`}>Trading Risk Alert</p>
           <p className={`${classPrefix}-main-text`}>
-            这是一个风险提示这是一个风险提示这是一个风险提示这是一个风险提示
+            Please note that "Limit order" or "Stop order" is not
+            guaranteed to be executed. This may include, but is not limited to,
+            the following cases.<br />
+            1. The trigger price has been reached but there is not enough time
+            to execute.<br />
+            2. No one picks up the execution order There may be slippage to
+            execute at the trigger price
           </p>
           <p className={`${classPrefix}-main-select`}>
             <button
