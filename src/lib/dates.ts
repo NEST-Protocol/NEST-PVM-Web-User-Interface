f8926aff95ab25033aeb2d1555a5ce5452bd7150
tunogya <tunogya@qq.com>
2023-04-05 16:52:26 +0800

    update

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -2,9 +2,9 @@ import { addMinutes, format as formatDateFn } from "date-fns";
 
 export function formatTVDate(date: Date) {
   // https://github.com/date-fns/date-fns/issues/1401#issuecomment-578580199
-  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "dd MMM yyyy");
+  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "yyyy-MM-dd");
 }
 
 export function formatTVTime(date: Date) {
-  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "H:mm");
+  return formatDateFn(addMinutes(date, date.getTimezoneOffset()), "H:mm:ss").replace(/:00$/, "");
 }
