df2699aefdf6d6e4990823ae8178c1404a6477d0
DoctorLi <doctorli2042@gmail.com>
2023-04-28 16:33:32 +0800

    Update LimitAndPriceModal.tsx

--- src/pages/Futures/Modal/LimitAndPriceModal.tsx
+++ src/pages/Futures/Modal/LimitAndPriceModal.tsx
@@ -19,20 +19,38 @@ const TriggerRiskModal: FC<TriggerRiskModalProps> = ({ ...props }) => {
       title={"Risk Warning"}
     >
       <p>
-        Contract that may cau bur principal to be damaged. Please evaluate the
-        risk yourself before deciding whether to participate.1. External oracle
-        risk: NEST protocol's perpetual contract price comes from the NEST
-        oracle. lf the oracle is attacked or other reasons cause the price to be
-        abnormal, the system may encounter settlement exceptions, which may
-        cause errors in the user'.
+        For users/smart contracts using NEST Protocol perpetual contracts,
+        please understand its rules and the differences of similar products in
+        the market when performing positions, settlement, liquidation and other
+        related operations. After fully understanding the following possible
+        risks and can bear Participate in the risk situation:
         <br />
-        1. External oracle risk: NEST protocol's perpetual contract price comes
+        1. Uncertainty risk of income: The calculation method of NEST perpetual
+        contract is different from the calculation method of perpetual contract
+        of traditional centralized exchange. The NEST perpetual contract
+        converts the future price of the asset based on the historical rate of
+        return of the asset. Therefore, the asset price increases by 100% after
+        the user opens a position. The user's income is not 100%. In extreme
+        cases, even if the asset price is rising, your income may be negative.
+        <br />
+        2. Position liquidation risk: NEST perpetual contracts provide leverage
+        of 1-20 times. When your leverage is greater than 1 times leverage, the
+        price fluctuation of the underlying asset may cause liquidation.
+        <br />
+        3. NEST Token price fluctuation risk: Both the margin and the final
+        profit and loss use NEST Token, and NEST itself is also a highly
+        volatile asset. In extreme cases, even if your perpetual contract
+        position is profitable, However, due to the fluctuation of the NEST
+        price itself, it may cause you to lose money in terms of fiat currency.
+        <br />
+        4. External oracle risk: NEST protocol's perpetual contract price comes
         from the NEST oracle. If the oracle is attacked or other reasons cause
-        the price to be abnormal, the system may encounter settlement
+        the price to be abnormal, The system may encounter settlement
         exceptions, which may cause errors in the user's revenue calculation.
         <br />
-        2. Smart contract risk: The smart contract of the NEST protocol has not
-        been audited. There may be fatal unknown sin the smart.
+        5. Smart contract risk: There may be fatal unknown risks in the smart
+        contract that may cause your principal to be damaged. Please evaluate
+        the risk yourself before deciding whether to participate.
         <br />
         <br />
         Please note that "Limit order" or "Stop order" is not guaranteed to be
