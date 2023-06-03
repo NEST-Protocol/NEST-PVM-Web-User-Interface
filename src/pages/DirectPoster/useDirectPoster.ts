3e18e7acd2f694e41af9b71f8151adc27f44e78c
DoctorLi <doctorli2042@gmail.com>
2023-06-03 14:29:34 +0800

    update

--- src/pages/DirectPoster/useDirectPoster.ts
+++ src/pages/DirectPoster/useDirectPoster.ts
@@ -4,17 +4,23 @@ import useNEST from "../../hooks/useNEST";
 import { useProvider } from "wagmi";
 import { BigNumber } from "ethers/lib/ethers";
 import { t } from "@lingui/macro";
+import { useScrollNESTfaucet } from "../../contracts/useScrollNEST";
+import { TransactionType, usePendingTransactions } from "../../hooks/useTransactionReceipt";
 
 export function useDirectPoster() {
+  const { isPendingType } = usePendingTransactions();
   const [ethBalance, setEthBalance] = useState<BigNumber>();
   const { account, chainsData, setShowConnect } = useNEST();
   const provider = useProvider();
+
   const { balance: nestBalance, balanceOfRefetch: nestBalanceRefetch } =
     useReadTokenBalance(
       ("NEST".getTokenAddress(chainsData.chainId) ??
         String().zeroAddress) as `0x${string}`,
       account.address ?? ""
     );
+    const { transaction: faucet } = useScrollNESTfaucet();
+
   const getETHBalance = useCallback(async () => {
     if (account.address) {
       const balance = await provider.getBalance(account.address);
@@ -29,12 +35,36 @@ export function useDirectPoster() {
       return t`GET` + ` NEST ` + t`Testnet token`;
     }
   }, [account.address]);
+  const pending = useMemo(() => {
+    return (
+      isPendingType(TransactionType.faucet_scroll)
+    );
+  }, [isPendingType]);
+  const mainButtonLoading = useMemo(() => {
+    if (
+      faucet.isLoading ||
+      pending
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }, [faucet.isLoading, pending]);
+  const mainButtonDis = useMemo(() => {
+    if (!account.address) {
+      return false;
+    }
+    return false
+  }, [account.address]);
   const mainButtonAction = useCallback(() => {
     if (mainButtonTitle === t`Connect Wallet`) {
       setShowConnect(true);
+    } else if (mainButtonLoading) {
+      return
     } else {
+      faucet.write?.()
     }
-  }, [mainButtonTitle, setShowConnect]);
+  }, [faucet, mainButtonLoading, mainButtonTitle, setShowConnect]);
 
   useEffect(() => {
     getETHBalance();
@@ -59,6 +89,8 @@ export function useDirectPoster() {
     showETHBalance,
     getNESTError,
     mainButtonTitle,
+    mainButtonLoading,
+    mainButtonDis,
     mainButtonAction,
   };
 }
