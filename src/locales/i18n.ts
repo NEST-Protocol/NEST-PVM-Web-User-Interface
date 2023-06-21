e4d06900dfe4a3cb90c3f9cc630a0dda72d044a6
tunogya <tunogya@qq.com>
2023-06-21 09:56:22 +0800

    update

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -4,7 +4,7 @@ import messagesJa from "./ja/messages.js"
 import messagesKo from "./ko/messages.js"
 import messagesPt from "./pt/messages.js"
 import messagesRu from "./ru/messages.js"
-// import messagesVi from "./vi/messages.js"
+import messagesVi from "./vi/messages.js"
 
 export const defaultLocale = "en";
 i18n.load('en', messagesEn)
@@ -12,7 +12,7 @@ i18n.load('ja', messagesJa)
 i18n.load('ko', messagesKo)
 i18n.load('pt', messagesPt)
 i18n.load('ru', messagesRu)
-// i18n.load('vi', messagesVi)
+i18n.load('vi', messagesVi)
 /**
  * We do a dynamic import of just the catalog that we need
  * @param locale any locale string
