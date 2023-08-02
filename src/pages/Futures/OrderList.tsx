2b1de755508f3633dd9865896955966a5e2eda62
DoctorLi <doctorli2042@gmail.com>
2023-08-02 18:03:47 +0800

    Update OrderList.tsx

--- src/pages/Futures/OrderList.tsx
+++ src/pages/Futures/OrderList.tsx
@@ -232,19 +232,19 @@ const FuturesOrderList: FC<FuturesOrderListProps> = ({ ...props }) => {
       <Stack direction={"row"} alignItems={"center"} spacing={"4px"}>
         <FuturesOrder />
         <p>
-          <Trans>Positions</Trans>
+          {/* <Trans>Positions</Trans> */}
         </p>
       </Stack>,
       <Stack direction={"row"} alignItems={"center"} spacing={"4px"}>
         <FuturesLimitOrder />
         <p>
-          <Trans>Orders</Trans>
+          {/* <Trans>Orders</Trans> */}
         </p>
       </Stack>,
       <Stack direction={"row"} alignItems={"center"} spacing={"4px"}>
         <HistoryIcon />
         <p>
-          <Trans>History</Trans>
+          {/* <Trans>History</Trans> */}
         </p>
       </Stack>,
     ];
