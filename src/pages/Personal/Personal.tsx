f1d4ff516a46e9a408fbb282ecdf3d611dc66ba6
DoctorLi <doctorli2042@gmail.com>
2023-08-10 10:50:45 +0800

    Update Personal.tsx

--- src/pages/Personal/Personal.tsx
+++ src/pages/Personal/Personal.tsx
@@ -148,11 +148,12 @@ const Personal = () => {
   const addModal = useMemo(() => {
     return (
       <>
-        {showDeposit ? (
+        {/* {showDeposit ? (
           <DepositModal open={true} onClose={() => setShowDeposit(false)} />
         ) : (
           <></>
-        )}
+        )} */}
+
         {showWithdraw ? (
           <WithDrawModal
             open={true}
@@ -173,14 +174,7 @@ const Personal = () => {
         )}
       </>
     );
-  }, [
-    addTransactionNotice,
-    getAssetsList,
-    setShowDeposit,
-    setShowWithdraw,
-    showDeposit,
-    showWithdraw,
-  ]);
+  }, [addTransactionNotice, getAssetsList, setShowWithdraw, showWithdraw]);
 
   useEffect(() => {
     if (!checkSigned) {
