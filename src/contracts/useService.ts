ee6e626ce9bf3a3c3a198cce667341417e4ed871
tunogya <tunogya@qq.com>
2023-09-04 16:15:47 +0800

    add freeze balance

--- src/contracts/useService.ts
+++ src/contracts/useService.ts
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import useNEST from "../hooks/useNEST";
-import { serviceAsset } from "../lib/NESTRequest";
+import {serviceAsset} from "../lib/NESTRequest";
 
 function useService() {
     const { account, chainsData, signature } = useNEST();
@@ -18,8 +18,25 @@ function useService() {
           }
         }
       }, [account.address, chainsData.chainId, signature]);
+
+    const block_balance = useCallback(async (back: (result: number) => void) => {
+      if (chainsData.chainId && account.address && signature) {
+        const balanceBase: { [key: string]: any } = await serviceAsset(
+          chainsData.chainId,
+          account.address,
+          { Authorization: signature.signature }
+        );
+        if (Number(balanceBase["errorCode"]) === 0) {
+          const value: { [key: string]: number } = balanceBase["value"] ?? 0;
+          const balance = value["block"];
+          back(balance)
+        }
+      }
+    }, [account.address, chainsData.chainId, signature]);
+
     return {
-      service_balance
+      service_balance,
+      block_balance
     }
 }
 
